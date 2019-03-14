1be84c8c15a3d6685498847707228cd93947e2ce
Jeff Scott Brown <brownj@objectcomputing.com>
2019-03-14 10:29:12 -0500

    Initialize the logger factory
    This does away with the log4j ClassNotFound exceptions.

--- app/src/main/java/com/example/hellomicronaut/GreetingApplication.java
+++ app/src/main/java/com/example/hellomicronaut/GreetingApplication.java
@@ -2,9 +2,17 @@ package com.example.hellomicronaut;
 
 import android.support.multidex.MultiDexApplication;
 
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.JdkLoggerFactory;
+
 public class GreetingApplication extends MultiDexApplication {
 
     private HelloMicronautLifecycleCallbacks callback;
+
+    public GreetingApplication() {
+        super();
+        InternalLoggerFactory.setDefaultFactory(JdkLoggerFactory.INSTANCE);
+    }
 
     @Override
     public void onCreate() {
