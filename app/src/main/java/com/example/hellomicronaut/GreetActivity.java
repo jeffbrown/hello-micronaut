1b4538d014b0fff4f8a34920c21260272dff1fd5
Jeff Scott Brown <brownj@objectcomputing.com>
2019-03-30 19:27:25 -0500

    introduce fields to reference components

--- app/src/main/java/com/example/hellomicronaut/GreetActivity.java
+++ app/src/main/java/com/example/hellomicronaut/GreetActivity.java
@@ -13,16 +13,19 @@ public class GreetActivity extends AppCompatActivity {
     @Inject
     GreetingClient greetingClient;
 
+    private EditText nameText;
+    private TextView greetingView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greet);
+
+        nameText = findViewById(R.id.nameText);
+        greetingView = findViewById(R.id.greetingView);
     }
 
     public void greetButtonClicked(View view) {
-        EditText nameText = findViewById(R.id.nameText);
-        TextView greetingView = findViewById(R.id.greetingView);
-
         String name = nameText.getText().toString();
 
         String greeting = greetingClient.getGreeting(name);
